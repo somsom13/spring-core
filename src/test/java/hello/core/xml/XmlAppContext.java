d2c816741dc5c2b4470508f7651f7dfbdd37cd6e
somsom13 <somin1113@naver.com>
2021-10-29 18:48:44 +0900

    ADD xml기반 Bean 등록

---
+++ src/test/java/hello/core/xml/XmlAppContext.java
@@ -0,0 +1,17 @@
+package hello.core.xml;
+
+import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class XmlAppContext {
+
+    @Test
+    void xmlAppContext(){
+        ApplicationContext ac=new GenericXmlApplicationContext("appConfig.xml"); //resources밑의 xml을 자동으로 불러와 저장
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
